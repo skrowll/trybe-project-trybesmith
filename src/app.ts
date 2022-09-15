6b828e2f13dfda195beaa91d2bc385aa199bd82f
skrowll <lucasdejesus.rocha@hotmail.com>
2022-09-15 03:49:05 -0300

    feat(app): add '/products'

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import productRouter from './routes/product.route';
+
 const app = express();
 
 app.use(express.json());
 
-export default app;
+app.use('/products', productRouter);
 
-// test
+export default app;
