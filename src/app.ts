c7519bd6c8142eb129d1200d79ee6aa95ff62806
skrowll <lucasdejesus.rocha@hotmail.com>
2022-09-19 22:01:45 -0300

    feat(app): add '/orders'

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import productRouter from './routes/product.route';
 import userRouter from './routes/user.route';
+import orderRouter from './routes/order.route';
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.json());
 
 app.use('/products', productRouter);
 app.use('/users', userRouter);
+app.use('/orders', orderRouter);
 
 export default app;
